ace9a1abf7c02f54327710c128659105492733d1
Piotr Marecki <pm@aimachina.io>
2019-10-19 08:54:15 +0200

    Example: Car -- statement

---
+++ examples/car.py
@@ -0,0 +1,9 @@
+
+
+"""
+Klasa Car ,
+pola: producer: str, type: str, year: int, milage: int
+dodać konstruktor
+dodać sensowny __str__
+dodać metodę "cheat_milage", która pozwoli cofnięcie licznika o `param` km, default 1000km
+"""
--- struktury/klasy.py
+++ struktury/klasy.py
@@ -1,29 +1,37 @@
-
-
 class User:
-    def __init__(self, name, rank):
+    def __init__(self, name: str, rank: int):
         # to jest konstruktor (czyli jak tworzyć instancje klasy)
         # `self` === `this`; tnz. instancja klasy
         # sposób poniżej to definiowanie pól (==zmiennych instancji)
         self.name = name
         self.rank = rank
 
-    #definiowanie funkcji == metod klasy
+    # definiowanie funkcji == metod klasy
     def __str__(self):
         return f'user nazwa:{self.name}, ranga:{self.rank}'
 
-    def greet(self):
-        print(f'hello {self.name}!')
+    def greet(self, second_message='', counter=0):
+        print(f'Hello {self.name}! {second_message}')
 
+
+def increase_rank(uu: User):
+    uu.rank += 1
 
 
 u1 = User('Xi', 1)
 u2 = User('Wu', 3)
 u3 = User('Deng', 2)
 
-
 print(u1)
 print(u2)
 print(u3)
 
+# increase_rank(u3)
+# print(u3)
+
+#
 u2.greet()  # uruchomienie metody klasy
+u2.greet(counter=12)
+u2.greet(second_message='Have a good day!')
+
+
