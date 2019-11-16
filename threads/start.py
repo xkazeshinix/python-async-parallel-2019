4f0ce555eed9c6b60bf024d6ed67b270e7b9bf37
Piotr Marecki <pm@aimachina.io>
2019-11-16 09:12:07 +0100

    wątki, ale przed odpalaniem w pętli

--- threads/start.py
+++ threads/start.py
@@ -8,15 +8,26 @@ class MyThread(threading.Thread):
         self.title = title
 
     def run(self):
+
         print(f'wątek o tytule {self.title}; nazwa:{threading.current_thread().name}')
-        time.sleep(1.3)
+        time.sleep(3)
         print(f'wątek o tytule {self.title}; nazwa:{threading.current_thread().name} ---- koniec')
 
 
 print(f'główny wątek: {threading.current_thread().name}')
 
+# start
+start = time.time()
+
 t1 = MyThread('pierwszy')
 t2 = MyThread('drugi')
 
+# ćwiczenie: uruchomić trzykrotnie MyThread, ale tak, by całość zakończyła operację w czasie < 4s
+#    (zakończyła, tzn. ostatnia linia "koniec" została wypisana do konsoli )
 t1.start()
+t2.start()
 t1.join()
+
+end = time.time()
+duration = end-start
+print(f'całkowity czas wykonania: {duration:.6f}s')
